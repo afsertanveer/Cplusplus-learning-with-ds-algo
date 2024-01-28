5df9f9dbfd34aa1a7fe544f8d7be5b000bcae2c0
unknown <uiu.afser@gmail.com>
2024-01-28 06:37:33 +0600

    advanced problem solved

---
+++ BinarySearch/FirstAndLastPositionOfKeyInArray.cpp
@@ -0,0 +1,50 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[],int n, int key){
+    
+    int s=0, e = n -1;
+    int mid = s + (e-s)/2 ;
+    int ans = -1 ;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans = mid;
+            e = mid - 1;
+        }else if( key > arr[mid] ){
+            s = mid + 1;
+        }else if( key < arr[mid] ){
+            e = mid - 1 ;
+        }
+        
+        mid = s + (e-s) / 2 ;
+    }
+    return ans ;
+}
+
+int lastOcc(int arr[],int n, int key){
+    
+    int s=0, e = n -1;
+    int mid = s + (e-s)/2 ;
+    int ans = -1 ;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans = mid;
+            s = mid + 1;
+        }else if( key > arr[mid] ){
+            s = mid + 1;
+        }else if( key < arr[mid] ){
+            e = mid - 1 ;
+        }
+        
+        mid = s + (e-s) / 2 ;
+    }
+    return ans ;
+}
+int main() {
+    // Write C++ code here
+   int even[5] = {1,2,3,3,5};
+    cout<<"First Occurance of 3 is "<<firstOcc(even, 5 , 3)<<endl;
+    cout<<"Last Occurance of 3 is "<<lastOcc(even, 5 , 3)<<endl;
+    return 0;
+}
